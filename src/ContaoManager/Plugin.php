552235f8c39287bb5789a3765658f865da381aee
2M1 <mobilecryptology.application@gmail.com>
2018-07-01 19:27:36 +0200

    ContaoManager compatibility

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Webteam\PizzaBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Webteam\PizzaBundle;
+
+class Plugin implements BundlePluginInterface
+{
+
+  /**
+  * {@inheritdoc}
+  */
+  public function getBundles(ParserInterface $parser)
+  {
+    return [
+      BundleConfig::create(WebteamPizzaBundle::class)
+        ->setLoadAfter([ContaoCoreBundle::class]),
+    ];
+  }
+}
